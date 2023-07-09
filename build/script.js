d2735a364239c7d5bb8aeb4390c864cc5738a577
Elia Marcantognini <elia.marcantognini@gmail.com>
2023-07-09 21:04:52 +0200

    Add other H tags to changeFont(size)

--- build/script.js
+++ build/script.js
@@ -1,6 +1,8 @@
 function changeFont(size){
     var el = [document.getElementsByTagName('p'), document.getElementsByTagName('li'),
     document.getElementsByTagName('h1'), document.getElementsByTagName('h2'),
+    document.getElementsByTagName('h3'), document.getElementsByTagName('h4'),
+    document.getElementsByTagName('h5'), document.getElementsByTagName('h6'),
     document.getElementsByClassName('ut'), document.getElementsByTagName('strong')];
     el.forEach(elements => {
         for (var j = 0; j < elements.length; j++) {
